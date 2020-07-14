fb728ecdfdf0eaa45a9024b4f72c5b2ddcd4a357
chai <49208740+ssumachai@users.noreply.github.com>
2020-07-14 16:46:48 -0700

    Update veggies.hpp

--- veggies.hpp
+++ veggies.hpp
@@ -1,7 +1,7 @@
 #ifndef __VEGGIES_HPP__
 #define __VEGGIES_HPP__
 
-#include "base.hpp"
+#include "ingredients.hpp"
 #include <vector>
 
 class Veggies : public Base{
@@ -9,14 +9,63 @@ class Veggies : public Base{
 		std::vector<Ingredients*> selection;
 	public:
 		Veggies(){
-			char input;
+			char input = '';
 			std::cout << "Tomatoes? (Y/N): ";
 			std::cin >> input;
 			if(input == 'Y' || input == 'y'){selection.push_back(new Ingredients("TOMATOES", 0.00));}
 			else if(input == 'N' || input == 'n'){selections.push_back(new Ingredients("NO TOMATOES", 0.00));} 
 			else{}
+			
+			std::cout << "Lettuce? (Y/N): ";
+			std::cin >> input;
+			if(input == 'Y' || input == 'y'){selection.push_back(new Ingredients("LETTUCE", 0.00));}
+			else if(input == 'N' || input == 'n'){selections.push_back(new Ingredients("NO LETTUCE", 0.00));} 
+			else{}
+			
+			std::cout << "Pickles? (Y/N): ";
+			std::cin >> input;
+			if(input == 'Y' || input == 'y'){selection.push_back(new Ingredients("PICKLES", 0.00));}
+			else if(input == 'N' || input == 'n'){selections.push_back(new Ingredients("NO PICKLES", 0.00));} 
+			else{}
+			
+			std::cout << "Onions? (Y/N): ";
+			std::cin >> input;
+			if(input == 'Y' || input == 'y'){selection.push_back(new Ingredients("ONIONS", 0.00));}
+			else if(input == 'N' || input == 'n'){selections.push_back(new Ingredients("NO ONIONS", 0.00));} 
+			else{}
 		}
-		Veggies(char *c[5]){}
+		Veggies(char (*c)[4]){				
+			char input = '';
+			
+			input = (*c)[0];
+			if(input == 'Y' || input == 'y'){selection.push_back(new Ingredients("TOMATOES", 0.00));}
+			else if(input == 'N' || input == 'n'){selections.push_back(new Ingredients("NO TOMATOES", 0.00));} 
+			else{}
+			
+			input = (*c)[1];
+			if(input == 'Y' || input == 'y'){selection.push_back(new Ingredients("LETTUCE", 0.00));}
+			else if(input == 'N' || input == 'n'){selections.push_back(new Ingredients("NO LETTUCE", 0.00));} 
+			else{}
+			
+			input = (*c)[2];
+			if(input == 'Y' || input == 'y'){selection.push_back(new Ingredients("PICKLES", 0.00));}
+			else if(input == 'N' || input == 'n'){selections.push_back(new Ingredients("NO PICKLES", 0.00));} 
+			else{}
+			
+			input = (*c)[3];
+			if(input == 'Y' || input == 'y'){selection.push_back(new Ingredients("ONIONS", 0.00));}
+			else if(input == 'N' || input == 'n'){selections.push_back(new Ingredients("NO ONIONS", 0.00));} 
+			else{}
+		}
+	virtual double price(){return 0.00};
+	virtual std::string receipt(){
+		std::string str = "";
+		for(unsigned i = 0; i < 3; i++){
+			str = str + selection.at(i)->receipt() + std::endl;
+		}
+		str = str + selection.at(3);
+		return str;
+	}
 };
 
 #endif // __VEGGIES_HPP__		
