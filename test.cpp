0c8880cdab0c78ecb27e27d334775b0e139e32af
ssumachai <ssumachai@gmail.com>
2020-07-14 17:18:20 -0700

    Created more Veggies Test

--- test.cpp
+++ test.cpp
@@ -4,11 +4,23 @@
 #include <iostream>
 
 TEST(VeggieTest, ArrayInput){
-	char array[4] = {'Y','Y','Y','Y'};
-	Veggies* test_val = new Veggies(array);
+	char array1[4] = {'Y','Y','Y','Y'};
+	Veggies* test1 = new Veggies(array1);
 
-	EXPECT_EQ(test_val->price(), 0);
-	EXPECT_EQ(test_val->receipt(), "TOMATOES\nLETTUCE\nPICKLES\nONIONS");
+	EXPECT_EQ(test1->price(), 0);
+	EXPECT_EQ(test1->receipt(), "TOMATOES\nLETTUCE\nPICKLES\nONIONS");
+
+	char array2[4] = {'N','N','N','N'};
+        Veggies* test2 = new Veggies(array2);                                                                                                   
+        
+	EXPECT_EQ(test2->price(), 0);                                             
+        EXPECT_EQ(test2->receipt(), "NO TOMATOES\nNO LETTUCE\nNO PICKLES\nNO ONIONS");
+
+        char array3[4] = {'Y','N','Y','N'};
+        Veggies* test3 = new Veggies(array3);                                                                                                   
+        
+	EXPECT_EQ(test3->price(), 0); 
+        EXPECT_EQ(test3->receipt(), "TOMATOES\nNO LETTUCE\nPICKLES\nNO ONIONS");          
 }
 
 int main(int argc, char **argv){
